9454866d14466b948bf65ded4e008bc56ee118a7
samarthsb4real <samarthbhadane23@gmail.com>
2024-07-29 22:17:16 +0530

    home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <main className="text-gray-700">
-      <h1 className="head-text text-white">Home</h1>
+      <h1 className="head-text text-white">Home Page</h1>
       <div className="text-xl">
         
       </div>
